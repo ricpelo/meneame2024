277df2095db62d33f234fbb32ce4c08089a72d48
Ricardo Pérez <ricpelo@gmail.com>
2024-02-05 20:37:21 +0100

    Crea relaciones entre modelos

--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Comentario extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comentable()
+    {
+        return $this->morphTo();
+    }
+
+    public function comentarios()
+    {
+        return $this->morphMany(Comentario::class, 'comentable');
+    }
 }
--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Noticia extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function etiquetas()
+    {
+        return $this->belongsToMany(Etiqueta::class);
+    }
+
+    public function meneadores()
+    {
+        return $this->belongsToMany(User::class, 'meneos');
+    }
+
+    public function comentarios()
+    {
+        return $this->morphMany(Comentario::class, 'comentable');
+    }
 }
